6413b3f6d2631e8e40eff5f5508ab560ac222b44
Antonio Furtado <antoniordf@hotmail.com>
2023-04-01 01:07:40 +0100

    amended migration files

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,5 @@
 const tonyToken = artifacts.require("tonyToken");
 
 module.exports = function (deployer) {
-  // The arguments for the constructor should be provided here
-  const _name = "Tony Token";
-  const _symbol = "TONY";
-  const _decimals = 18;
-  const _initialSupply = 100000;
-
-  deployer.deploy(tonyToken, _name, _symbol, _decimals, _initialSupply);
+  deployer.deploy(tonyToken, "Tony Token", "TONY", 18, 100000);
 };
